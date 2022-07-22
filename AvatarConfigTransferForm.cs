ab45957e067e297cd3cc305f61e0c8197b2062ff
MistressPlague <krewellamusicps3@gmail.com>
2022-07-22 01:28:20 +0100

    Add project files.

---
+++ AvatarConfigTransferForm.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AvatarConfigTransfer
+{
+    public partial class AvatarConfigTransferForm : Form
+    {
+        public AvatarConfigTransferForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AvatarConfigTransferForm_Load(object sender, EventArgs e)
+        {
+            GetUsers();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            GetUsers();
+        }
+
+        private void GetUsers()
+        {
+            comboBox1.Items.Clear();
+
+            if (Directory.Exists(textBox1.Text))
+            {
+                foreach (var userid in Directory.GetDirectories(textBox1.Text).Select(Path.GetFileName))
+                {
+                    comboBox1.Items.Add(userid);
+                }
+            }
+
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(textBox2.Text))
+            {
+                MessageBox.Show("File Not Found!");
+                return;
+            }
+
+            if (!Directory.Exists(textBox1.Text + "\\" + comboBox1.Text))
+            {
+                MessageBox.Show("Path Not Found!");
+                return;
+            }
+
+            File.Copy(textBox2.Text, textBox1.Text + "\\" + comboBox1.Text + "\\" + Path.GetFileName(textBox2.Text), true);
+
+            MessageBox.Show("Done!");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(textBox1.Text + "\\" + comboBox1.Text))
+            {
+                MessageBox.Show("Path Not Found!");
+                return;
+            }
+
+            Process.Start(textBox1.Text + "\\" + comboBox1.Text);
+        }
+    }
+}
---
+++ VRCAPI.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvatarConfigTransfer
+{
+    internal class VRCAPI
+    {
+        internal class UnityPackage
+        {
+            internal string assetUrl;
+            internal int assetVersion;
+            internal DateTime created_at;
+            internal string id;
+            internal string platform;
+            internal string pluginUrl;
+            internal long unitySortNumber;
+            internal string unityVersion;
+        }
+
+        internal class AvatarInfo
+        {
+            internal string assetUrl;
+            internal string authorId;
+            internal string authorName;
+            internal DateTime created_at;
+            internal string description;
+            internal bool featured;
+            internal string id;
+            internal string imageUrl;
+            internal string name;
+            internal string releaseStatus;
+            internal string[] tags;
+            internal string thumbnailImageUrl;
+            internal string unityPackageUrl;
+            internal UnityPackage[] unityPackages;
+            internal DateTime updated_at;
+            internal int version;
+        }
+    }
+}
